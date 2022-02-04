080821130bff82b214466a938250f1813f3ae925
Marcelo J Forclaz <marcelo.forclaz@stensul.com>
2022-02-04 16:59:57 -0300

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 class FunkyCSV {
     constructor(options = {
@@ -54,6 +63,22 @@ class FunkyCSV {
             _filename += '.csv';
         }
         return _filename;
+    }
+    write(filename) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!process) {
+                throw new Error('Write method is not available in current environment.');
+            }
+            return new Promise((resolve, reject) => {
+                const fs = require('fs');
+                fs.writeFile(this.parseFilename(filename), this.getCsv(), (error) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve();
+                });
+            });
+        });
     }
     download(filename) {
         if (!document) {
